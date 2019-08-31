fd0da745ae15a626ba9470eb7f10f1d410de9bf1
Hawk Shannon <hawk.dillinger@gmail.com>
2019-08-31 17:22:05 -0400

    added nav bar

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Nav () {
+  return (
+   <nav className= "navbar navbar-expand-lg">
+      <a className="navbar-brand" href="/">
+      GOOGLE BOOKS SEARCH 
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
